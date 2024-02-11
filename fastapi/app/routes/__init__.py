a64665c8bdac0e2071b2fdb726b126e84f499492
Owen <33508519+lxxonx@users.noreply.github.com>
2024-02-11 23:27:24 +0900

    fastapi: day 13

--- fastapi/app/routes/__init__.py
+++ fastapi/app/routes/__init__.py
@@ -7,6 +7,7 @@ from .day_7 import day_7_router
 from .day_8 import day_8_router
 from .day_11 import day_11_router, static_router
 from .day_12 import day_12_router
+from .day_13 import day_13_router
 from .example import example_router
 
 router = APIRouter()
@@ -20,6 +21,7 @@ router.include_router(router=day_7_router)
 router.include_router(router=day_8_router)
 router.include_router(router=day_11_router)
 router.include_router(router=day_12_router)
+router.include_router(router=day_13_router)
 
 __all__ = [
     "router",
